68d0c37a9adf9fb8446df8d970aac88a146e6ef7
data-lee <ndoucet67@gmail.com>
2016-02-17 00:23:33 -0600

    created functions for get deck, shuffle deck and deal

---
+++ cards.php
@@ -0,0 +1,69 @@
+<?php
+
+/** 
+* Create a standard 52 deck of cards
+* @return array
+*/
+function getDeck() {
+	$deck = [];
+	$suits = ['D', 'C', 'H', 'S'];
+	$ranks = array_merge(['A'], range(2, 10), ['J', 'Q', 'K']);
+	foreach ($suits as $suit) {
+		foreach ($ranks as $rank) {
+			$deck[] = $rank . $suit;
+		}
+	}
+	return $deck;
+}
+
+/** 
+* Shuffles a deck of cards.
+* @param array of cards
+* @return randomized array
+*/
+function shuffleDeck(&$deck) {
+	shuffle($deck);
+}
+
+/*
+* 
+*/
+function deal($players, $numCards, &$deck) {
+	// convert players from values to keys
+	$playersHands = array_flip($players);
+	// set each player key to an empty array
+	foreach ($players as $player) {
+		$playersHands[$player] = [];
+	}
+	// deal the cards
+	$count = $numCards - 1;
+	foreach (range(0, $count) as $i) {
+		foreach ($players as $player) {
+			$currentCard = array_shift($deck);
+			$playersHands[$player][$i] = $currentCard;	
+		}
+	}
+	
+	return $playersHands;
+	return $deck;
+}
+
+
+// testing variables
+$deck = getDeck();
+shuffleDeck($deck);
+
+$players = [
+	'Natalia',
+	'Hubert',
+	'Jeborah',
+	'Baskets'
+];
+$numCards = 5;
+
+$firstHand = deal($players, $numCards, $deck);
+print_r($deck);
+print_r($firstHand);
+
+
+?>
